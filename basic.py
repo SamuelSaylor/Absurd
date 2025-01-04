17aee15404f4b7116291c61eeddc62a763b22003
SamuelSaylor <samuelsaylor2025@gmail.com>
2025-01-03 18:31:07 -0600

    Update basic.py

--- basic.py
+++ basic.py
@@ -17,6 +17,10 @@ class Error: # Errors in case stuff goes down
 class IllegalCharError (Error): # When lexer finds error it dont support
     def __init__(self, pos_start, pos_end, details):
         super().__init__(pos_start, pos_end, "There is an issue with your computer, I am pondering the possibilities and believe it might just be an illusive \"IllegalCharError\" which basically has some very simple implications! I am led to believe it is because... uhhhhh... i forgot. Google is free though! Here's some lame details: ", details)
+
+class InvalidSyntaxError(Error):
+    def __init__(self, pos_start, pos_end, details=""):
+        super().__init__(pos_start,pos_end,"Well this is peculiar, there is an error but Id rather not discuss it with you. Think about your mistake, and try again. If that doesnt work, try again! Keep fixing mistakes, this one will keep coming in! Heres a HUGE hint for this error: Its very common and its because you messed up horribly. Either improve and stop making mistakes, or just get better.",details)
 #################################################
 
 class Position: # Pinpoints position in code where stuff happens
@@ -127,9 +131,83 @@ class Lexer: #breaks down code into tokens
         else:
             # float
             return Token(TT_FLOAT, float(num_str))
-        
+
+# NODES ###################################
+
+class NumberNode:
+    def __init__(self, token):
+        self.token = token
+    
+    def __repr__(self):
+        return f'{self.token}'
+
+class BinOpNode: # Operations such as addition, subtraction, etc
+    def __init__(self, left_node, op_tok, right_node):
+        self.left_node = left_node
+        self.op_tok = op_tok
+        self.right_node = right_node
+
+    def __repr__(self):
+        return f'({self.left_node}, {self.op_tok}, {self.right_node})'
+
+# Parser############################
+
+class Parser: # A parser takes data and breaks it into smaller parts. For example: 7 + 10 * 2 becomes 7 + (10 * 2)
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.tok_index = -1
+        self.advance()
+
+    def advance(self):
+        self.tok_index += 1
+        if self.tok_index < len(self.tokens):
+            self.current_token = self.tokens[self.tok_index]
+        return self.current_token
+
+# Parser functions#####################################
+
+    def parse(self):
+        res = self.expr()
+        return res
+    
+    def factor(self): # In math, a number, variable, term, or longer expression that is multiplied by something else
+        token = self.current_token
+
+        if token.type in (TT_INT, TT_FLOAT):
+            self.advance()
+            return NumberNode(token)
+
+    def term(self): # In math, a number variable or constant multiplied by a variable or variables
+        return self.bin_op(self.factor, (TT_MUL, TT_DIV))
+
+    def expr(self): # Expression: Combination of one or more terms that an assembler evaluates into a single value
+        return self.bin_op(self.term, (TT_PLUS, TT_MINUS))
+
+    ###########################################
+
+    def bin_op(self, func, ops): # function it refers to, binary operation
+        left = func()
+
+        while self.current_token.type in ops:
+            op_tok = self.current_token
+            self.advance()
+            right = func()
+            left = BinOpNode(left, op_tok, right)
+
+        return left # RETURNS LEFT FACTOR, AKA WHAT IS TO LEFT OF NUM
+
+
 def run(fn, text): # Run stuff
+    # Gen tokens
     lexer = Lexer(fn, text)
     tokens, error = lexer.make_tokens()
+    if error:
+        return None, error
     
+    # Gen AST
+    parser = Parser(tokens)
+    ast = parser.parse()
+    
+    return ast, None
+
     return tokens, error
